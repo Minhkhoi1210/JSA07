792f2706e9b47775c833fdbcd9fdf2b4b639ab64
Minhkhoi1210 <101977853+Minhkhoi1210@users.noreply.github.com>
2022-05-17 18:01:14 +0700

    checkpoint

---
+++ Lesson 6(check point)/script.js
@@ -0,0 +1,16 @@
+
+
+
+// bài 1
+let Student = ["nguyen minh khoi", "15", "bong ro", "the estella"];
+console.log(Student);
+
+
+
+// bài 2
+var smartPhones = [ 
+    { name: 'iphone', price: 649 }, 
+    { name: 'Galaxy S6', price: 576 }, 
+    { name: 'Galaxy Note 5', price: 489 } 
+];
+console.log(smartPhones.map(smartPhone => smartPhone.price)); 
